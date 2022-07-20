de7a6314bc9f78e8b71a08f2ae05e69af4ba12c8
Joshua Zahner <joshuazahner@gmail.com>
2022-07-20 12:02:49 -0700

    Updated .dat file writing routing to not skip constant lines

--- R/utils/fun_write_dat.R
+++ R/utils/fun_write_dat.R
@@ -9,8 +9,8 @@ fun_write_dat <- function(dat.files, catches, obs, params, ss, year){
   
   options(scipen=10)
 
-  if(-9 %in% obs$seine.age.comp){ss$seine <- 0}
-  if(-9 %in% obs$spawn.age.comp){ss$spawn <- 0}
+  if(-9 %in% obs$seine.age.comp){ss$seac <- 0}
+  if(-9 %in% obs$spawn.age.comp){ss$spac <- 0}
 
   # Write main data file with actual catches (known w/ error)
   # and observed survey values (from fun_obsm.R).
@@ -18,9 +18,9 @@ fun_write_dat <- function(dat.files, catches, obs, params, ss, year){
   # Add new data 
   PWS.ASA.dat[[1]]  <- PWS.ASA.dat[[1]]+1                                         # Number of year in data
   PWS.ASA.dat[[2]]  <- PWS.ASA.dat[[2]]+1                                         # Number of years to be fit to
-  # 3 is number of age classes (10). Needs no change.
+  PWS.ASA.dat[[3]]  <- 10                                                         # Number of age classes (constant)
   PWS.ASA.dat[[4]]  <- rbind(PWS.ASA.dat[[4]],  obs$waa)                          # Weight-at-age (assumed constant 03/2022) 
-  PWS.ASA.dat[[5]]  <- rbind(PWS.ASA.dat[[5]],  rep(-9, 10))                      # Fecundity (no data post 1997) 
+  PWS.ASA.dat[[5]]  <- rbind(PWS.ASA.dat[[5]],  obs$fec)                          # Fecundity (no data post 1997) 
   PWS.ASA.dat[[6]]  <- rbind(PWS.ASA.dat[[6]],  catches[3, ])                     # Pound catch fishery
   PWS.ASA.dat[[7]]  <- params$pk                                                  # Prop of pound-catch killed (Assumed constant)
   PWS.ASA.dat[[8]]  <- rbind(PWS.ASA.dat[[8]],  catches[4, ])                     # Foodbait fishery
@@ -31,9 +31,9 @@ fun_write_dat <- function(dat.files, catches, obs, params, ss, year){
   PWS.ASA.dat[[12]] <- rbind(PWS.ASA.dat[[12]], obs$mdm)                          # Observed MDM
   PWS.ASA.dat[[13]] <- rbind(PWS.ASA.dat[[13]], obs$egg)                          # Observed egg deposition
   PWS.ASA.dat[[14]] <- rbind(PWS.ASA.dat[[14]], obs$egg.sd)                       # Observed egg deposition sd
-  # 15 is first year of ADFG hydro survey. Needs no change.
+  PWS.ASA.dat[[15]] <- 26                                                         # First year of ADFG hydroacoustic survey (constant)
   PWS.ASA.dat[[16]] <- rbind(PWS.ASA.dat[[16]], obs$adfg.hydro)                   # Observed ADFG hydroacoustic survey biomass
-  # 17 is first year of PWSSC hydro survey. Needs no change.
+  PWS.ASA.dat[[17]] <- 14                                                         # FIrst year of PWSSC hydroacoustic survey (constant)
   PWS.ASA.dat[[18]] <- rbind(PWS.ASA.dat[[18]], obs$pwssc.hydro)                  # Observed PWSSC hydroacoustic survey biomass
   PWS.ASA.dat[[19]] <- rbind(PWS.ASA.dat[[19]], obs$pwssc.hydro.sd)               # Observed PWSSC hydroacoustic survey biomass sd
   PWS.ASA.dat[[20]] <- rbind(PWS.ASA.dat[[20]], obs$seine.age.comp)               # Observed seine age composition
@@ -68,8 +68,8 @@ fun_write_dat <- function(dat.files, catches, obs, params, ss, year){
   # Write age comp sample sizes file with actual 
   # sample sizes (currently fixed).
   age.comp.samp.sizes.txt <- dat.files$agecomp_samp_sizes.txt
-  age.comp.samp.sizes.txt[[1]] <- rbind(age.comp.samp.sizes.txt[[1]], ss$seine)
-  age.comp.samp.sizes.txt[[2]] <- rbind(age.comp.samp.sizes.txt[[2]], ss$spawn)
+  age.comp.samp.sizes.txt[[1]] <- rbind(age.comp.samp.sizes.txt[[1]], ss$seac)
+  age.comp.samp.sizes.txt[[2]] <- rbind(age.comp.samp.sizes.txt[[2]], ss$spac)
   age.comp.samp.sizes.txt[[3]] <- rbind(age.comp.samp.sizes.txt[[3]], 0)
   age.comp.samp.sizes.txt[[4]] <- rbind(age.comp.samp.sizes.txt[[4]], 0)
   # Write file without headers
