b45d1d3cf90b268007d29f41221543cb626825f6
Joshua Zahner <joshuazahner@gmail.com>
2022-12-12 13:03:44 -0600

    Updated control rule functions for HCR testing

--- R/operating_model/control_rules/cr_constant_f.R
+++ R/operating_model/control_rules/cr_constant_f.R
@@ -1,8 +1,5 @@
-hcr.constant.f <- function(ssb.projected, naa.projected, 
-                           options=list(
-                               f.rate = 0.0
-                           )){
+hcr.constant.f <- function(..., options=list(f.rate=0.0)){
 
-    return(ssb.projected*options$f.rate)                
+    return(options$f.rate)                
 
 }
