0be1eb10f3b2d7b959d540c051803b68a9deef44
Joshua Zahner <joshuazahner@gmail.com>
2022-12-21 15:53:02 -0600

    Turne off generation of SEAC data as it inhibited convergence

--- R/operating_model/fun_obsm.R
+++ R/operating_model/fun_obsm.R
@@ -6,7 +6,7 @@
 library(gtools)
 
 fun_obsm <- function(survey.indices, waa, fec, perc.female, juv.overdisp, sample.sizes, y, sim.seed,
-                     survey.controls = list(mdm=TRUE, egg=FALSE, pwssc.hydro=TRUE, adfg.hydro=FALSE, spac=TRUE, seac=TRUE, juv=TRUE, vhsv=FALSE, ich=FALSE)){
+                     survey.controls = list(mdm=TRUE, egg=FALSE, pwssc.hydro=TRUE, adfg.hydro=FALSE, spac=TRUE, seac=FALSE, juv=TRUE, vhsv=FALSE, ich=FALSE)){
   
   set.seed(sim.seed)
 
@@ -38,7 +38,7 @@ fun_obsm <- function(survey.indices, waa, fec, perc.female, juv.overdisp, sample
   spawn.age.comp <- rmultinom(1, sample.sizes$spac, spawn.age.comp.predicted[y, ]/sum(spawn.age.comp.predicted[y, ]))
   spawn.age.comp <- t(spawn.age.comp/sum(spawn.age.comp))
 
-  if(any(seine.age.comp.predicted==-9)){
+  if(any(seine.age.comp.predicted==-9) | survey.controls$seac == FALSE){
     seine.age.comp <- rep(-9, ncol(seine.age.comp.predicted))
   }else{
     #seine.age.comp <- gtools::rdirichlet(1, 119*seine.age.comp.predicted[y, ])
@@ -53,7 +53,7 @@ fun_obsm <- function(survey.indices, waa, fec, perc.female, juv.overdisp, sample
   egg.sd <- ifelse(survey.controls$egg, egg.sd, -9)
 
   #aerial.juv <- -9 # negative binomial error distribution (check formula)
-  aerial.juv <- ifelse(survey.controls$juv, rnbinom(1, size=juv.overdisp, mu=survey.indices$juv.schools), -9)
+  aerial.juv <- ifelse(survey.controls$juv, rnbinom(1, size=juv.overdisp, mu=survey.indices$juv.schools[y]), -9)
   vhsv.antibody <- ifelse(survey.controls$vhsv, vhsv.antibody.predicted[y, ], rep(-9, 20)) # ask John for simulation code for these
   ich.antibody  <- ifelse(survey.controls$ich,  ich.antibody.predicted[y, ], rep(-9, 20))
 
