7e4eb43d62a3ddacb2abe3139c8c2541b067fa09
Joshua Zahner <joshuazahner@gmail.com>
2023-01-23 09:47:56 -0900

    Better tradeoff plots

--- R/plotting/catch_biomass_tradeoff.R
+++ R/plotting/catch_biomass_tradeoff.R
@@ -14,9 +14,9 @@ fnames <- c("foodbait_catch.csv", "gillnet_catch.csv", "pound_catch.csv", "seine
 nyr <- 25
 years <- seq(1980, 1980+42+nyr-1)
 
-sims <- c(197, 2255, 2386, 3709, 4716, 8388, 8904, 8634, 8935, 1094, 4288)
-
-hcr.names <- c("base", "high.harvest", "low.harvest", "high.biomass", "low.biomass", "evenness", "gradient", "three.step.thresh", "big.fish", "constant.f.00")
+sims <- c(197, 649, 1017, 1094, 1144, 1787, 1998, 2078, 2214, 2241, 2255, 2386, 3169, 3709, 4288, 4716, 4775, 6460, 7251, 7915, 8004, 8388, 8462, 8634, 8789, 8904, 8935, 9204, 9260, 9716, 9725)
+nyr <- 25
+hcr.names <- c("base", "low.harvest", "high.harvest", "low.biomass", "high.biomass", "constant.f.00", "evenness", "gradient", "three.step.thresh", "big.fish")
 
 model.dir <- here::here("results/base/sim_197/year_25/model/")
 
@@ -28,7 +28,7 @@ for(cr in hcr.names){
     print(cr)
     cr.dat <- read.catch.data(cr, sims, nyr)
     catch.data <- catch.data %>% bind_rows(cr.dat)
-    biomass.dat <- read.biomass.data(cr, sims, 25)
+    biomass.dat <- read.true.biomass.data(cr, sims, 25)
     bio.traj.df <- bio.traj.df %>% bind_rows(biomass.dat)
     # for(s in sims){
     #     #model.dir <- paste0(here::here("results/"), cr, "/sim_", s, "/year_25/model/")
@@ -94,16 +94,16 @@ catch.biomass.df <- as_tibble(catch.biomass.df) %>%
                         select(-c(n, n.below)) %>%
                         group_by(control.rule) %>%
                         median_qi(
-                            tot_catch, biomass, depletion, aav, prob.below,
+                            tot_catch, ann_catch, biomass, depletion, aav, prob.below,
                             .width=c(0.5, 0.95)
                         )
-catch.biomass.df$cr <- factor(catch.biomass.df$cr, 
+catch.biomass.df$cr <- factor(catch.biomass.df$control.rule, 
                                 levels=c("base", "high.harvest", "low.harvest", "lower.b0", "low.biomass", "higher.b0", "high.biomass", "evenness", "gradient", "three.step.thresh", "constant.f.00", "big.fish"),
                                 labels=c("Default", "High F", "Low F", "Low B0", "Low Threshold", "High B0", "High Threshold", "Evenness", "Gradient", "Three-Step Threshold", "No Fishing", "Big Fish Only"))
 
 colnames(catch.biomass.df)[1] <- "control.rule"
 
-tradeoff.df.long <- reformat.metric.df("tot_catch") %>%
+tradeoff.df.long <- reformat.metric.df("ann_catch") %>%
                         bind_rows(
                             reformat.metric.df("biomass"),
                             reformat.metric.df("depletion"),
@@ -112,6 +112,7 @@ tradeoff.df.long <- reformat.metric.df("tot_catch") %>%
                         )
 
 fit.tradeoff.line <- function(formula, xrange, name){
+
     lm.model <- lm(formula, data=catch.biomass.df[catch.biomass.df$.width == 0.5,])
     newdata <- data.frame(x=xrange)
     colnames(newdata) <- c(name)
@@ -137,24 +138,27 @@ generate.tradeoff.plot <- function(vars){
     d <- d1 %>% inner_join(d2, by=c("control.rule", ".width"))
 
     axis.breaks <- list(
-        tot_catch = seq(0, 500000, 50000),
-        biomass = seq(0, 150000, 10000),
-        depletion = seq(0, 5, 0.5),
-        aav = seq(1, 0, -0.1),
+        tot_catch = seq(-50000, 550000, 50000),
+        ann_catch = seq(-5000, 30000, 5000),
+        biomass = seq(-10000, 160000, 10000),
+        depletion = seq(-1, 5, 1),
+        aav = round(seq(1.2, -0.2, -0.2), 2),
         prob.below = seq(0.5, 0, -0.05)
     )
 
     axis.labels <- list(
-        tot_catch = seq(0, 500, 50),
-        biomass = seq(0, 150, 10),
-        depletion = seq(0, 5, 0.5),
-        aav = seq(1, 0, -0.1),
+        tot_catch = seq(-50, 550, 50),
+        ann_catch = seq(-5, 30, 5),
+        biomass = seq(-10, 160, 10),
+        depletion = seq(-1, 5, 1),
+        aav = round(seq(1.2, -0.2, -0.2), 2),
         prob.below = seq(0.5, 0, -0.05)
     )
 
     metric.names <- list(
-        tot_catch = "Total Catch (10000 mt)",
-        biomass = "Final Year Biomass (10000 mt)",
+        tot_catch = "Total Catch (1000 mt)",
+        ann_catch = "Annual Catch (1000 mt)",
+        biomass = "Final Year Biomass (1000 mt)",
         depletion = "Final Year Depletion Level",
         aav = "Average Annual Catch Variation",
         prob.below = "Proportion of Years Biomass Below Lower Regulatory Threshold"
@@ -162,6 +166,7 @@ generate.tradeoff.plot <- function(vars){
 
     metric.names.short <- list(
         tot_catch = "Total Catch",
+        ann_catch = "Annual Catch",
         biomass = "Final Year Biomass",
         depletion = "Final Year Depletion",
         aav = "Catch Variation",
@@ -180,23 +185,33 @@ generate.tradeoff.plot <- function(vars){
     x.axis.name <- metric.names[[vars[1]]]
     y.axis.name <- metric.names[[vars[2]]]
 
+    model <- fit.tradeoff.line(as.formula(paste0(vars[2], "~", vars[1])), min(x.breaks):max(x.breaks), name=vars[1])
+
     plot <- ggplot(d)+
+            geom_smooth(aes(x=x, y=y, ymin=ymin, ymax=ymax), data=model$preds, stat="identity", color="black")+
+            geom_point(aes(x=median.x, y=median.y, color=control.rule), size=4)+
             geom_pointinterval(aes(x=median.x, xmin=lower.x, xmax=upper.x,
                                 y=median.y,
-                                color=control.rule))+
+                                color=control.rule), alpha=0.33)+
             geom_pointinterval(aes(x=median.x,
                                 y=median.y, ymin=lower.y, ymax=upper.y,
-                                color=control.rule))+
-            #geom_smooth(aes_auto(model$preds), data=model$preds, stat="identity", color="black")+
+                                color=control.rule), alpha=0.33)+
+            scale_color_manual(values=hcr.colors.named, name="Control Rule") + 
             scale_x_continuous(x.axis.name, breaks=x.breaks, labels=x.labels)+
             scale_y_reverse(y.axis.name, breaks=y.breaks, labels=y.labels)+
             coord_cartesian(
-                xlim=c(x.breaks[1], x.breaks[length(x.breaks)]), 
-                ylim=c(y.breaks[1], y.breaks[length(y.breaks)]), 
-                expand=c(0.1, 0.1)
+                xlim=c(x.breaks[2], x.breaks[length(x.breaks)-1]), 
+                ylim=c(y.breaks[2], y.breaks[length(y.breaks)-1]), 
+                expand=c(0.1,0.1)
             )+
-            ggtitle(plot.title) + 
+            ggtitle(plot.title) +
+            theme_minimal()+ 
             theme(
+                axis.line.x = element_line(),
+                axis.line.y = element_line(),
+                axis.text = element_text(size=14),
+                axis.title = element_text(size=18, face="bold"),
+                plot.title = element_blank(),
                 panel.grid.minor = element_blank()
             )
 
@@ -205,13 +220,13 @@ generate.tradeoff.plot <- function(vars){
     return(plot)
 }
 
-generate.tradeoff.plot(c("biomass", "tot_catch"))
-generate.tradeoff.plot(c("biomass", "aav"))
-generate.tradeoff.plot(c("tot_catch", "aav"))
-generate.tradeoff.plot(c("tot_catch", "prob.below"))
+dvc.trade.plot <- generate.tradeoff.plot(c("depletion", "ann_catch"))
+dvv.trade.plot <- generate.tradeoff.plot(c("depletion", "aav"))
+cvv.trade.plot <- generate.tradeoff.plot(c("ann_catch", "aav"))
+#generate.tradeoff.plot(c("tot_catch", "prob.below"))
 
 library(patchwork)
-(cvb.toff.plot+guides(color=FALSE) | aavvb.toff.plot+guides(color=FALSE) | aavvc.toff.plot)
+(dvc.trade.plot| dvv.trade.plot| cvv.trade.plot) + plot_layout(guides="collect") & theme(legend.position="top-right")
 
 
 cvb.lm.model   <- lm(tot_catch ~ biomass,     data=catch.biomass.df %>% filter(.width == 0.5) %>% select(tot_catch, biomass))
