aeca6450defa8eb5a4d7361900f22819ea44d519
Joshua Zahner <joshuazahner@gmail.com>
2023-03-02 08:15:09 -0800

    perf comp_biomass_traj: parallel data reading

--- R/plotting/plot_composite_biomass_trajectory.r
+++ R/plotting/plot_composite_biomass_trajectory.r
@@ -8,8 +8,9 @@ library(tidyverse)
 source(paste0(here::here("R/utils"), "/fun_read_dat.R"))
 source(paste0(here::here("R/plotting"), "/plot_util_vals.R"))
 
-nyr <- 30
+nyr <- 25
 years <- seq(1980, 1980+42+nyr-1)
+sims <- c(197, 649, 1017, 1094, 1144, 1787, 1998, 2078, 2214, 2241, 2255, 2386, 2512, 3169, 3709, 4050, 4288, 4716, 4775, 6460, 7251, 7915, 8004, 8388, 8462, 8634, 8789, 8904, 8935, 9204, 9260, 9716, 9725)
 
 set.seed(1)
 sims <- sample(1:1e4, size=40)
@@ -20,7 +21,7 @@ sims <- c(sims, sample(1:1e4, size=40))
 
 hcr.names <- c("base", "high.harvest", "low.harvest", "high.biomass", "low.biomass", "evenness", "gradient", "three.step.thresh", "big.fish", "constant.f.00")
 
-year.0.fname <- paste0(here::here("results/base/sim_526/year_0/model/mcmc_out/"), "PFRBiomass.csv")
+year.0.fname <- paste0(here::here("results/base/sim_197/year_0/model/mcmc_out/"), "PFRBiomass.csv")
 curr.biomass.data <- read_csv(year.0.fname, col_names=as.character(years)) %>% pivot_longer(everything(), names_to="year", values_to="biomass")
 curr <- data.frame(
     year=as.numeric(rep(pull(select(curr.biomass.data, "year")), length(hcr.names))), 
@@ -29,12 +30,17 @@ curr <- data.frame(
     sim=0
 )
 
-biomass.traj.raw <- data.frame(year=NA, biomass=NA, control.rule=NA, sim=NA) 
-for(cr in hcr.names){
-    print(cr)
-    biomass.traj.raw <- biomass.traj.raw %>% bind_rows(read.true.biomass.data(cr, seeds, nyr))
-}
-biomass.traj.raw
+cores <- parallel::detectCores()
+cl <- makeCluster(min(cores[1]-1, length(hcr.names)), outfile="")
+registerDoParallel(cl)
+
+biomass.traj.raw <- pbapply::pblapply(hcr.names, function(cr, seeds, nyr){
+    source(file=paste0(here::here("R/utils/"), "fun_read_dat.R"))
+    biomass.dat <- read.true.biomass.data(cr, seeds, nyr)
+}, seeds=sims, nyr=nyr, cl=cl)
+biomass.traj.raw <- bind_rows(biomass.traj.raw)
+
+stopCluster(cl)
 
 biomass.traj <- biomass.traj.raw %>% na.omit() %>% 
                     bind_rows(curr) %>%
@@ -57,7 +63,7 @@ p1 <- ggplot(biomass.traj, aes(x=year, y=biomass, color=control.rule, group=cont
         geom_hline(yintercept = 20000, linetype="longdash")+
         geom_hline(yintercept = 40000, linetype="longdash")+
         scale_fill_grey(start=0.8, end=0.6)+
-        scale_x_continuous("Year", breaks=seq(1980, 2022+nyr, by=5), expand=c(0,0))+
+        scale_x_continuous("Year", breaks=seq(1980, 2022+nyr, by=10), expand=c(0,0))+
         scale_y_continuous("Pre-Fishery Biomass (1000 mt)", breaks=c(0, 20000, 40000, 50000, 100000, 150000, 200000), labels=c(0, 20, 40, 50, 100, 150, 200), expand=c(0,0))+
         coord_cartesian(ylim=c(0, 200000))+
         ggtitle("Median Biomass Trajectories under Different Control Rules")+
@@ -65,7 +71,9 @@ p1 <- ggplot(biomass.traj, aes(x=year, y=biomass, color=control.rule, group=cont
             panel.grid.minor = element_blank(),
             panel.background = element_blank(),
             axis.line.x = element_line(),
-            axis.line.y = element_line()
+            axis.line.y = element_line(),
+            axis.text.x = element_text(size=14),
+            axis.text.y = element_text(size=14),
         )
 p1
 
@@ -104,15 +112,17 @@ p2 <- ggplot(dynamic.b0.raw %>% filter(year >= 2022), aes(x=year, y=dyn.b0, colo
         geom_vline(xintercept=2022, linetype="longdash")+
         geom_vline(xintercept=2022+15, linetype="longdash")+
         scale_color_manual(values=as.vector(hcr.colors))+
-        scale_x_continuous("Year", breaks=seq(1980, 2022+nyr, by=5), expand=c(0,0))+
-        scale_y_continuous("Biomass Relative to Unfished Conditions", breaks=seq(0.0, 1.2, 0.1), expand=c(0,0))+
-        coord_cartesian(ylim=c(0, 1.2))+
+        scale_x_continuous("Year", breaks=seq(1980, 2022+nyr, by=10), expand=c(0,0))+
+        scale_y_continuous("Biomass Relative to Unfished Conditions", breaks=seq(0.0, 1.0, 0.1), expand=c(0,0))+
+        coord_cartesian(ylim=c(0, 1.05))+
         ggtitle("Dynamic Biomass Trajectories under Different Control Rules")+
         theme(
           panel.grid.minor = element_blank(),
           panel.background = element_blank(),
           axis.line.x = element_line(),
-          axis.line.y = element_line()
+          axis.line.y = element_line(),
+          axis.text.x = element_text(size=14),
+          axis.text.y = element_text(size=14)
         )
 p2
 
